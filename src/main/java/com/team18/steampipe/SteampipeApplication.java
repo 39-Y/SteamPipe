4f164c831bef027c3724eb6fd8dbdc67257de61a
Beconfident01 <hom0726@naver.com>
2023-05-24 17:39:31 +0900

    세팅

---
+++ src/main/java/com/team18/steampipe/SteampipeApplication.java
@@ -0,0 +1,13 @@
+package com.team18.steampipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteampipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteampipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team18/steampipe/SteampipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team18.steampipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteampipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
